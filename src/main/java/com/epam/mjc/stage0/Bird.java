609ff538fad8c82db964f5b9986e507b7993eadf
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-12-04 18:33:15 +0100

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+   
+    public Bird() {
+        super("blue", 2, false);
+    }
 
+    @Override
+    public String getDescription() {
+        // Call the parent class method
+        String baseDescription = super.getDescription();
+
+        // Add additional information for Bird
+        return baseDescription + " Moreover, it has 2 wings and can fly.";
+    }
+
+   
 }
